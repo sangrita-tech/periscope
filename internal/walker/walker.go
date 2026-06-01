cd4648b632bf3ca0c6b1bc627d008ed90563569e
pounce00x <pounce00x@example.com>
2026-06-01 21:13:29 +0500

    walker

--- internal/walker/walker.go
+++ internal/walker/walker.go
@@ -4,7 +4,6 @@ import (
 	"io/fs"
 	"path"
 
-	"github.com/rs/zerolog/log"
 	"github.com/sangrita-tech/periscope/internal/domain"
 )
 
@@ -17,53 +16,11 @@ func New() *Walker {
 func (w *Walker) Walk(fsys fs.FS, root string) ([]domain.Entry, error) {
 	root = path.Clean(root)
 
-	meta, err := fs.Stat(fsys, root)
-	if err != nil {
-		return nil, err
-	}
-
-	if !meta.IsDir() {
-		return w.walkSingleFile(fsys, root, meta)
-	}
-
-	entries, err := w.walkDirectory(fsys, root)
-	if err != nil {
-		return nil, err
-	}
-
-	return entries, nil
-}
-
-func (w *Walker) walkSingleFile(
-	fsys fs.FS,
-	filePath string,
-	meta fs.FileInfo,
-) ([]domain.Entry, error) {
-	data, err := fs.ReadFile(fsys, filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return []domain.Entry{
-		{
-			Path:    filePath,
-			RelPath: path.Clean(filePath),
-			Data:    data,
-			Meta:    meta,
-		},
-	}, nil
-}
-
-func (w *Walker) walkDirectory(fsys fs.FS, root string) ([]domain.Entry, error) {
 	var entries []domain.Entry
 
-	err := fs.WalkDir(fsys, root, func(currentPath string, dirEntry fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if currentPath == root {
-			return nil
+	err := fs.WalkDir(fsys, root, func(currentPath string, dirEntry fs.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
 		}
 
 		if dirEntry.IsDir() {
@@ -80,14 +37,14 @@ func (w *Walker) walkDirectory(fsys fs.FS, root string) ([]domain.Entry, error) 
 			return err
 		}
 
+		currentPath = path.Clean(currentPath)
+
 		entries = append(entries, domain.Entry{
 			Path:    currentPath,
-			RelPath: path.Clean(currentPath),
+			RelPath: currentPath,
 			Data:    data,
 			Meta:    meta,
 		})
-
-		log.Info().Str("path", currentPath).Msg("Found file")
 
 		return nil
 	})
